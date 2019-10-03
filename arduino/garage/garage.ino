5451d6657cc03604efb033beb0da42cdf64f3d2f
Bertin Colpron <bcolpron78@gmail.com>
2019-10-02 22:11:01 -0400

    wip

--- arduino/garage/garage.ino
+++ arduino/garage/garage.ino
@@ -1,6 +1,11 @@
 
 unsigned long last;
 
+unsigned request_size = 0;
+char request_buf[32];
+
+#define INTERVAL 3000
+
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
@@ -9,16 +14,46 @@ void setup() {
 
 void send(const char* command, int val)
 {
-    char s[1024];
-    sprintf(s, "RESPONSE state=%d\n", val);
-    Serial.print(s);
+    Serial.print('[');
+    Serial.print(millis());
+    Serial.print("] ");
+    Serial.print(command);
+    Serial.print(" state=");
+    Serial.println(val);
+    //Serial.flush();
+}
+
+bool readRequest(char* buf, unsigned maxSize)
+{
+  //assert(maxSize >= 2);
+  if (!Serial.available()) return false;
+  int c = Serial.read();
+  if ( c == 10 || c == 13)
+  {
+    buf[request_size] = '\0';
+    request_size = 0;
+    return true;
+  }
+  if (request_size >= maxSize -1)
+  {
+    memcpy(request_buf, request_buf+1, maxSize-2);
+    request_size--;
+  }
+  buf[request_size++] = c;
+  return false;
 }
 
 void loop() {
-  if(millis() > last+10000)
+  if(millis() > last+INTERVAL)
   {
     send("UPDATE", digitalRead(2));
     last = millis();
+  }
+
+  if (readRequest(request_buf, sizeof request_buf))
+  {
+    Serial.println(request_buf);
+    send("RESPONSE", digitalRead(2));
   }
 }
 
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <algorithm>
 #include <iterator>
-
+#include <regex>
 
 std::string read(std::istream &stream, std::string::size_type count)
 {
@@ -12,12 +12,23 @@ std::string read(std::istream &stream, std::string::size_type count)
     return out;
 }
 
-int main(int, const char**)
+int main(int, const char **)
 {
     std::ifstream f("/dev/ttyACM0");
-    
-    auto s = read(f, 100);
-    std::cout << s << std::endl;
+    f.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
+
+    std::regex re(".*RESPONSE state=(0|1)");
+
+    while (true)
+    {
+        auto s = read(f, 20);
+        std::cout << "<" << s << ">" << std::endl;
+
+        std::smatch matches;
+        if (std::regex_search(s, matches, re)) {
+            std::cout << matches[1] << std::endl;
+        }
+    }
 
     return 0;
 }
